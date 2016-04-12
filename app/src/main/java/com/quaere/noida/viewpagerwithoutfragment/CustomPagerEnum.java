e89348c13f971643b09ffc72c903574675b202de
deepak <deepaksachan8@gmail.com>
2016-04-12 15:16:39 +0530

    Initial commit

---
+++ app/src/main/java/com/quaere/noida/viewpagerwithoutfragment/CustomPagerEnum.java
@@ -0,0 +1,28 @@
+package com.quaere.noida.viewpagerwithoutfragment;
+
+/**
+ * Created by QServer on 4/12/2016.
+ */
+public enum CustomPagerEnum {
+
+    RED(R.string.red, R.layout.view_red),
+    BLUE(R.string.blue, R.layout.view_blue),
+    ORANGE(R.string.orange, R.layout.view_orange);
+
+    private int mTitleResId;
+    private int mLayoutResId;
+
+    CustomPagerEnum(int titleResId, int layoutResId) {
+        mTitleResId = titleResId;
+        mLayoutResId = layoutResId;
+    }
+
+    public int getTitleResId() {
+        return mTitleResId;
+    }
+
+    public int getLayoutResId() {
+        return mLayoutResId;
+    }
+
+}
---
+++ app/src/main/java/com/quaere/noida/viewpagerwithoutfragment/MainActivity.java
@@ -0,0 +1,99 @@
+package com.quaere.noida.viewpagerwithoutfragment;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new CustomPagerAdapter(this));
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class CustomPagerAdapter extends PagerAdapter {
+
+        private Context mContext;
+
+        public CustomPagerAdapter(Context context) {
+            mContext = context;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup collection, int position) {
+            CustomPagerEnum customPagerEnum = CustomPagerEnum.values()[position];
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            ViewGroup layout = (ViewGroup) inflater.inflate(customPagerEnum.getLayoutResId(), collection, false);
+            collection.addView(layout);
+
+      /*      CustomPagerEnum dd = CustomPagerEnum.values()[position];
+            String s =mContext.getString(customPagerEnum.getTitleResId());
+            Toast.makeText(mContext, s, Toast.LENGTH_LONG).show();*/
+            return layout;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup collection, int position, Object view) {
+            collection.removeView((View) view);
+        }
+
+        @Override
+        public int getCount() {
+            return CustomPagerEnum.values().length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            CustomPagerEnum customPagerEnum = CustomPagerEnum.values()[position];
+
+
+            return mContext.getString(customPagerEnum.getTitleResId());
+
+
+        }
+
+    }
+
+}
